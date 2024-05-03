aa8b72c319cbf008af4b4dd93adabcb5eb284a58
MeganAe <168326500+MeganAe@users.noreply.github.com>
2024-05-04 00:30:45 +0200

    Update ask.js

--- scripts/cmds/ask.js
+++ scripts/cmds/ask.js
@@ -1,17 +1,18 @@
 const axios = require('axios');
 
 const Prefixes = [
-  'ask', 
+  'AI',
   'ai',
-  'ghost' 
+  'megan',
+  'Ai',
 ];
 
 module.exports = {
   config: {
-    name: "ask",
+    name: "ai",
     version: 1.0,
-    author: "Naruto",
-    longDescription: "AI", 
+    author: "fatkey",
+    longDescription: "chatGptAI",
     category: "ai",
     guide: {
       en: "{p} questions",
@@ -27,27 +28,19 @@ module.exports = {
       }
       const prompt = event.body.substring(prefix.length).trim();
    if (!prompt) {
-        await message.reply(" ༐⸙.. 𝗚𝗛𝗢𝗦𝗧..✨👻 ╼╴╼╴╼╴╼╴╼╴╼╴╼╴╼╴         🕊𝚈𝙾 𝙵𝚁𝙰𝙽𝙶𝙸𝙽 𝙹𝙴 𝚂𝚄𝙸𝚂 𝚃𝙾𝙽 𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃🤖 𝚅𝙸𝚁𝚃𝚄𝙴𝙻..🫶... 𝙿𝙾𝚂𝙴 𝙼𝙾𝙸 𝚃𝙰 𝚀𝚄𝙴𝚂𝚃𝙸𝙾𝙽 ༅    ╼╴╼╴╼╴╼╴╼╴╼╴╼╴╼╴               ༈ ࿂┉༐⸙ 𝗛𝗔𝗜𝗧𝗔𝗡𝗜.....❍♱")
+        await message.reply("📝 𝗪𝗮𝗹𝗸𝗲𝗿𝗚𝗣𝗧\n━━━━━━━━━━━━━━\n\nHello! How can I assist you today.⚪");
         return;
       }
 
 
-      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=hello${encodeURIComponent(prompt)}`);
       const answer = response.data.answer;
 
  
-    await message.reply({ body: `➬.༐☄️👻 𝗚𝗛𝗢𝗦𝗧  ༐🪶🩷....✰
-▭▭▭▭▭▭▭▭▭▭▭▭
+    await message.reply(`📝 𝗪𝗮𝗹𝗸𝗲𝗿𝗚𝗣𝗧:\n━━━━━━━━━━━━━━\n${answer}\n━━━━━━━━━━━━━━\n🟢 𝘔𝘦𝘵𝘰𝘶𝘴𝘩𝘦𝘭𝘢 𝘸𝘢𝘭𝘬𝘦𝘳 ⚪`);
 
-
-${answer}
-
-
-▢▢▢✞྿..𝗛𝗔𝗜𝗧𝗔𝗡𝗜..྿✞▢▢▢`,
-});
-
-   } catch (error) {
+    } catch (error) {
       console.error("Error:", error.message);
-    }uw
+    }
   }
-      }
+};
